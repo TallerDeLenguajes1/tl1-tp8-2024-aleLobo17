1b0ca3680b572fe5fbb71ffb0e09e4933a9b8271
Lobo <lobo.rodrigoalejandro.2020@gmail.com>
2024-08-05 21:56:00 -0300

    x/

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using EspacioTarea;
+List<Tarea> pendientes = new List<Tarea>();
+List<Tarea> realizadas = new List<Tarea>();
+FuncionesTarea fc = new FuncionesTarea();
+fc.crearTareas(pendientes);
+int opcion;
+do
+{
+    Console.WriteLine("1. Mostrar lista.");
+    Console.WriteLine("2. Mover de pendientes a realizadas.");
+    Console.WriteLine("3. Buscar por descripcion.");
+    Console.WriteLine("4. Salir.");
+    if (int.TryParse(Console.ReadLine(), out opcion))
+    {
+        switch (opcion)
+        {
+            case 1:
+                Console.WriteLine("Lista Pendientes: ");
+                fc.mostrarTareas(pendientes);
+                Console.WriteLine("Lista Realizadas: ");
+                fc.mostrarTareas(realizadas);
+                break;
+            case 2:
+                fc.MoverTarea(pendientes, realizadas);
+                break;
+            case 3:
+                Console.Write("Ingrese una descripcion para buscar en la lista. ");
+                fc.BuscarTareasPorDescripcion(Console.ReadLine(), pendientes);
+                break;
+            case 4:
+                Console.WriteLine("Saliendo...");
+                break;
+            default:
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Error. valor invalido");
+        Console.WriteLine("Intente otra vez...");
+    }
+
+} while (opcion!=4);
---
+++ Tarea.cs
@@ -0,0 +1,99 @@
+﻿using System;
+namespace EspacioTarea
+{
+
+    public class Tarea
+    {
+
+        private int idTarea;
+        public int IdTarea { get => idTarea; set => idTarea = value; }
+        private int duracion;
+        public int Duracion { get => duracion; set => duracion = value; }
+        private string descripcion;
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+
+        public Tarea(int idTarea, int duracion, string descripcion)
+        {
+            this.idTarea = idTarea;
+            this.duracion = duracion;
+            this.descripcion = descripcion;
+        }
+
+        public Tarea()
+        {
+            descripcion=" ";
+        }
+        public void MostrarTarea()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"ID de Tarea: {IdTarea}, Duración: {Duracion}, Descripción: {Descripcion}");
+            Console.ResetColor();
+        }
+    }
+    public class FuncionesTarea
+    {
+        public void crearTareas(List<Tarea> pendientes)
+        {
+            Random rand = new Random();
+            int n = rand.Next(10, 101);
+            for (int i = 0; i < n; i++)
+            {
+                pendientes.Add(new Tarea
+                {
+                    IdTarea = i,
+                    Descripcion = $"Tarea {i}",
+                    Duracion = rand.Next(10, 101)
+                });
+            }
+        }
+        public void mostrarTareas(List<Tarea> lista)
+        {
+            foreach (Tarea i in lista)
+            {
+                i.MostrarTarea();
+            }
+        }
+        public void MoverTarea(List<Tarea> pendientes, List<Tarea> realizadas)
+        {
+            Console.Write("Por favor, ingresa el ID de la tarea que deseas mover: ");
+            int idTarea;
+            if (int.TryParse(Console.ReadLine(), out idTarea))
+            {
+                Tarea tarea = pendientes.FirstOrDefault(t => t.IdTarea == idTarea);
+
+                if (tarea != null)
+                {
+                    pendientes.Remove(tarea);
+                    realizadas.Add(tarea);
+                }
+                else
+                {
+                    Console.WriteLine($"No se encontró ninguna tarea pendiente con el ID {idTarea}.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El ID ingresado no es válido. Por favor, intenta de nuevo.");
+            }
+        }
+
+        public void BuscarTareasPorDescripcion(string descripcion, List<Tarea>pendientes)
+        {
+            List<Tarea> tareasEncontradas = pendientes.Where(t => t.Descripcion == descripcion).ToList();
+
+            if (tareasEncontradas.Count > 0)
+            {
+                Console.WriteLine($"Se encontraron {tareasEncontradas.Count} tareas con la descripción \"{descripcion}\":");
+                foreach (Tarea tarea in tareasEncontradas)
+                {
+                    tarea.MostrarTarea();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No se encontraron tareas con la descripción \"{descripcion}\".");
+            }
+        }
+
+    }
+}
